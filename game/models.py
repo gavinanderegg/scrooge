088125a3413a5a30188e898f3f8ca17e386772c6
Gavin Anderegg <gavin@anderegg.ca>
2017-12-17 18:41:35 -0400

    Making this work standalone

--- game/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Vote(models.Model):
-    choice = models.CharField(max_length=32)
-    ip = models.IPAddressField()
-    time = models.DateTimeField(auto_now=True)
